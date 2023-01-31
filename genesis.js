b5e959ab9f43053bb05ef704530c8ffcd695c0c3
jshvaron <jshva@icloud.com>
2023-01-31 17:22:30 -0600

    edits prompts to hit acceptance criteria

--- genesis.js
+++ genesis.js
@@ -2,39 +2,44 @@ const inquirer = require('inquirer');
 
 inquirer.prompt([
     {
-        type: '',
-        name: '',
-        message: '',
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
-        name: 'answer',
-        message: 'Is this a test?',
+        name: 'desctiption',
+        message: 'How would you describe the project?',
     },
     {
         type: 'input',
-        name: 'answer',
-        message: 'Is this a test?',
+        name: 'contents',
+        message: 'What are the table of contents?',
     },
     {
         type: 'input',
-        name: 'answer',
-        message: 'Is this a test?',
+        name: 'install',
+        message: 'How does the user install thos application?',
     },
     {
         type: 'input',
-        name: 'answer',
-        message: 'Is this a test?',
+        name: 'usage',
+        message: 'How does someone use this project?',
     },
     {
         type: 'input',
-        name: 'answer',
-        message: 'Is this a test?',
+        name: 'liscence',
+        message: 'What license(s) does this project have?',
     },
     {
         type: 'input',
-        name: 'answer',
-        message: 'Is this a test?',
+        name: 'contribution',
+        message: 'Please provide guidlines for open source contribution.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do we test this app?',
     },
 
 
