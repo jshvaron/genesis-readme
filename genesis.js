1a6f9daadcf70e45fec3dd0aa50c4d5605805d2d
jshvaron <jshva@icloud.com>
2023-01-31 17:09:34 -0600

    sets up prompt structures

--- genesis.js
+++ genesis.js
@@ -2,14 +2,45 @@ const inquirer = require('inquirer');
 
 inquirer.prompt([
     {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
         type: 'input',
         name: 'answer',
         message: 'Is this a test?',
+    },
+    {
+        type: 'input',
+        name: 'answer',
+        message: 'Is this a test?',
+    },
+    {
+        type: 'input',
+        name: 'answer',
+        message: 'Is this a test?',
+    },
+    {
+        type: 'input',
+        name: 'answer',
+        message: 'Is this a test?',
+    },
+    {
+        type: 'input',
+        name: 'answer',
+        message: 'Is this a test?',
+    },
+    {
+        type: 'input',
+        name: 'answer',
+        message: 'Is this a test?',
+    },
 
-    }
+
 ]).then((answers) => {
     if (answers.answer === 'yes') {
-        console.log(' are you sure????');
+        console.log('STILL A BUSTUH');
 
     }}
 );
